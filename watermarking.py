f38b6d16e6f21ad26b8498711a2e3a5d23d75407
May-Thae-Sue <misue.mts@gmail.com>
2019-10-27 11:27:44 +0630

    Update watermarking.py

--- watermarking.py
+++ watermarking.py
@@ -1,13 +1,15 @@
 # importing the required modules 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import PyPDF2
-import io
+import io, sys
 import datetime
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch
 import re
+
+#Usage: python3 watermarking.py "author_name" file_name
   
 def add_watermark(wmFile, pageObj): 
 
@@ -15,58 +17,30 @@ def add_watermark(wmFile, pageObj):
     return pageObj 
   
 def main(): 
+
+    f = sys.argv[1]
+    origFileName = sys.argv[2]
     
-#    origFileName = 'merged_output.pdf'
-    origFileName = 'Chapter_1.pdf' 
-    newFileName = 'output.pdf'
     
     pdfFileObj = open(origFileName, 'rb') 
+    pdfWriter = PyPDF2.PdfFileWriter()
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    text = pdfReader.getPage(0).extractText()
-#    print(text)
-    x = re.sub("\n+", "", text)
-#    print(x)
-    searchObj = re.search( r'BY.*THESIS', x, re.I)
-    if searchObj:
-        extracted_string = searchObj.group()
-#       print(extracted_string)
-        author = re.sub(r'BY', "", extracted_string)
-#       print(author)
-        author = re.sub(r'B.E. THESIS', "", author)
-#       print(author)
-        author = re.sub(r'\s{2,10}', "",author)
-#       print(author)
-        pdfWriter = PyPDF2.PdfFileWriter()
-        packet = io.BytesIO()
-        can = canvas.Canvas("./watermark.pdf", pagesize=A4)
-        width, height = A4
-        time = datetime.datetime.today()
-        date = time.strftime("%h-%d-%Y %H:%M:%S")
-        can.setFont("Times-Roman", 12)
-        can.rotate(90)
-        can.drawString(0.5*inch, -inch, '''Created by %s at Date: %s''' % ( author, date))
-        can.save()
-        packet.seek(0)
-        mywatermark = PdfFileReader("watermark.pdf")
+    packet = io.BytesIO()
+    can = canvas.Canvas(packet, pagesize=A4)
+    width, height = A4
+    time = datetime.datetime.today()
+    date = time.strftime("%h-%d-%Y %H:%M:%S")
+    can.setFont("Times-Roman", 10)
+    can.rotate(90)
+    can.drawString(0.5*inch, -inch, '''Created by %s at Date: %s''' % ( f, date))
+    can.save()
+    packet.seek(0)
+    mywatermark = PdfFileReader(packet)
         
-    else:
-        pdfWriter = PyPDF2.PdfFileWriter()
-        packet = io.BytesIO()
-        can = canvas.Canvas("./watermark.pdf", pagesize=A4)
-        width, height = A4
-        time = datetime.datetime.today()
-        date = time.strftime("%h-%d-%Y %H:%M:%S")
-        can.setFont("Times-Roman", 12)
-        can.rotate(90)
-        can.drawString(0.5*inch, -inch, '''Created at Date: %s''' % (date))
-        can.save()
-        packet.seek(0)
-        mywatermark = PdfFileReader("watermark.pdf")               
-     
     for page in range(pdfReader.numPages): 
         wmpageObj = add_watermark(mywatermark, pdfReader.getPage(page)) 
         pdfWriter.addPage(wmpageObj) 
-    newFile = open(newFileName, 'wb') 
+    newFile = open(origFileName, 'wb') 
     pdfWriter.write(newFile) 
     pdfFileObj.close() 
     newFile.close() 
